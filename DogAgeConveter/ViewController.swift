5f7c44fb291f5fb2e1d9bd0376a5bd46a4227647
phollyphlox <phollyphlox@gmail.com>
2015-01-30 11:37:48 -0600

    Rev 1.0
    First commit

--- DogAgeConveter/ViewController.swift
+++ DogAgeConveter/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtDogAge: UITextField!
+    
+    @IBOutlet weak var lblDogAge: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnConvert(sender: UIButton) {
+        
+        txtDogAge.resignFirstResponder()
+        
+        var dogYears = 0.0
+        let humanAge = Double((txtDogAge.text as NSString).doubleValue)
+        
+        
+        if(humanAge > 2){
+            dogYears = (10.5 * 2) + ((humanAge - 2) * 4)
+        }
+        else{
+            dogYears = 10.5 * humanAge
+        }
+        lblDogAge.text = "\(dogYears)"
+    }
 
 }
 
